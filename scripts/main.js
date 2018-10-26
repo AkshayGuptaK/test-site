d7482586f2090de62a9932c910eb587b6c6cd3ca
Akshay <akshaygrkesh@gmail.com>
2018-10-26 11:06:08 +0530

    Created index page with navbar for other projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,3 @@
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!')
-}
-
 var myImage = document.querySelector('img')
 
 myImage.onclick = function() {
@@ -11,26 +7,4 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/blackout-main.png')
     }
-}
-
-var myButton = document.querySelector('button')
-var myHeading = document.querySelector('h1')
-
-var displayString = 'Werewolf: Blackout\nWelcome '
-
-function setUserName() {
-  var myName = prompt('Please enter your name.')
-  localStorage.setItem('name', myName)
-  myHeading.textContent = displayString + myName
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName()
-} else {
-  var storedName = localStorage.getItem('name')
-  myHeading.textContent = displayString + storedName
-}
-
-myButton.onclick = function() {
-  setUserName()
 }
